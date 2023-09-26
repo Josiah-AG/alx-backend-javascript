81c2b7e25fdb2cf22e3e86a2ddeb3a34ab6fe527
Eyoaiyas Gudeta <realgaz.eys@gmail.com>
2023-09-26 17:34:28 +0000

    task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,5 @@
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
-}
+export default function uploadPhoto(filename) {
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
+}}
